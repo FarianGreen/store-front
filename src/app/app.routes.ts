117b9f30004154ff1b6ce384b50a33c5af7b333e
Artem <artemfarafonov6@gmail.com>
2025-05-29 21:44:34 +0300

    feat(init): init app, created catalog, instaled angular animation and material

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { Catalog } from './features/catalog/pages/catalog';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+//   {
+//     path: '',
+//     loadComponent: () =>
+//       import('./home/home.component').then((m) => m.HomeComponent),
+//   },
+  {
+    path: 'catalog',
+    component: Catalog,
+  },
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
+import { RouterModule, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatButtonModule, RouterModule],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
 export class App {
   protected title = 'storefront';
